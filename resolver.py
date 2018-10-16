f1ef3b788f69d3548a21906a9f134c3e9cf135f4
WebSEC.nl <rebitclown105@hotmail.com>
2018-10-16 12:46:14 +0200

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -6,6 +6,7 @@ banner = ("#############################\n"
           "#############################")
 print(banner)
 print('')
-file = raw_input("PATH TO FILE: \n")
+file = raw_input("PATH TO FILE: ")
+print('\nRESOLVED: ')
 string = os.system('more < ' + str(file) + ':Zone.Identifier')
 raw_input('\nPRESS ANY KEY TO CONTINIUE')
